79bfb5f94dba77a45a575fbd714af92dcec1224c
cyberkid05 <cyberadithya05@gmail.com>
2022-05-24 21:51:33 +0530

    Time: 101 ms (20.72%), Space: 42.6 MB (26.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -1,11 +1,13 @@
-class Solution {
-    public int[] runningSum(int[] nums) {
-        int summ = 0;
-        int ans[] = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            summ += nums[i];
-            ans[i] = summ;
-        }
-        return ans;
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let summ = 0
+    let ans = Array(nums.length)
+    for (let i = 0; i < nums.length; i++) {
+        summ += nums[i]
+        ans[i] = summ
     }
-}
+    return ans
+};
