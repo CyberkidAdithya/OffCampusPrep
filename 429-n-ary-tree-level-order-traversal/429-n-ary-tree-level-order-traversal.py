19a2f4ce5c869efb299aa31af610d3b7a326353e
cyberkid05 <cyberadithya05@gmail.com>
2022-05-25 08:16:03 +0530

    Time: 104 ms (9.43%), Space: 16.2 MB (11.94%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -1,43 +1,25 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
 
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
-class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    public List<List<Integer>> levelOrder(Node root) {
-        if (root != null) {
-            Queue<Node> que = new LinkedList<>();
-            que.add(root);
-            while (que.size() > 0) {
-                int sz = que.size();
-                List<Integer> sub = new ArrayList<>();
-                for (int i = 0; i < sz; i++) {
-                    Node curr = que.remove();
-                    if (curr != null) {
-                        sub.add(curr.val);
-                        for (int j = 0; j < curr.children.size(); j++) {
-                            que.add(curr.children.get(j));
-                        }
-                    }
-                }
-                ans.add(sub);
-            }
-        }
-        return ans;
-    }
-}
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        ans = []
+        if root:
+            que = [root,]
+            while que:
+                sz = len(que)
+                print(len(que))
+                sub = []
+                for i in range(sz):
+                    ele = que.pop(0)
+                    sub.append(ele.val)
+                    for y in ele.children:
+                        if y:
+                            que.append(y)
+                ans.append(sub)
+        return ans
