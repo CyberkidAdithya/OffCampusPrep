77bfb48a2a9e27cf5f1142e46dd1729f0bd7bf67
cyberkid05 <cyberadithya05@gmail.com>
2022-05-25 08:53:48 +0530

    Time: 328 ms (38.38%), Space: 17.8 MB (64.90%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -1,44 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    int summ = 0;
-    public int deepestLeavesSum(TreeNode root) {
-        if (root != null) {
-            Queue<TreeNode> que = new LinkedList<>();
-            que.add(root);
-            while (que.size() > 0) {
-                int sz = que.size();
-                int presum = 0;
-                List<Integer> sub = new ArrayList<>();
-                for (int i = 0; i < sz; i++) {
-                    TreeNode curr = que.remove();
-                    sub.add(curr.val);
-                    presum += curr.val;
-                    if (curr.left != null) {
-                        que.add(curr.left);
-                    }
-                    if (curr.right != null) {
-                        que.add(curr.right);
-                    }
-                }
-                ans.add(sub);
-                summ = presum;
-            }
-        }
-        return summ;
-    }
-}
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Queue:
+    que = []
+    def __init__(self):
+        self.que = []
+    def enqueue(self, nbr):
+        self.que.append(nbr)
+    def dequeue(self):
+        return self.que.pop(0)
+    def __len__(self):
+        return len(self.que)
+    
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        que = []
+        anss = 0
+        if root:
+            levels = []
+            que.append(root)
+            while que:
+                sz = len(que)
+                sub = []
+                for i in range(sz):
+                    x = que.pop(0)
+                    sub.append(x.val)
+                    if x.left:
+                        que.append(x.left)
+                    if x.right:
+                        que.append(x.right)
+                levels.append(sub)
+                anss = sum(sub)
+        return anss
