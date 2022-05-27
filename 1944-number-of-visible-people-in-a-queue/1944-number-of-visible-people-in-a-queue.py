8bd8ba9709f14c9973b0b0fde5d2741c00c18b3e
cyberkid05 <cyberadithya05@gmail.com>
2022-05-27 16:01:18 +0530

    Time: 1786 ms (28.87%), Space: 29.8 MB (23.17%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -2,12 +2,12 @@ class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
         n = len(heights)
         ans = [0] * n
-        st = []
-        for i in range(n-1, -1, -1):
-            while st and heights[i] > st[-1]:
-                st.pop()
-                ans[i] += 1
-            if st:
-                ans[i] += 1
-            st.append(heights[i])
+        stack = []  # monotonic stack
+        for i in range(n - 1, -1, -1):
+            while stack and heights[i] > stack[-1]:
+                stack.pop()
+                ans[i] += 1 # smaller heights are popped until we encounter a bigger element
+            if stack:
+                ans[i] += 1 # rest of the stack elements to the right will be hidden by this element
+            stack.append(heights[i])
         return ans
