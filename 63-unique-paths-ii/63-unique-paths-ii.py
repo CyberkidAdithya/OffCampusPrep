a2381492771d57cd079a811a3746207fcf7248bd
cyberkid05 <cyberadithya05@gmail.com>
2022-06-01 15:41:11 +0530

    Time: 56 ms (59.71%), Space: 14.2 MB (20.08%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,31 @@
+import sys
+class Solution:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        maxx = sys.maxsize
+        dp = [[0] * n for _ in range(m)]    # make the grid
+        
+        dp[0][0] = 0 if grid[0][0] else 1
+        for i in range(1, m):  # base condition: to reach 1st row, the robot SHOULD MOVE IN RIGHT ONLY
+            dp[i][0] = 0 if grid[i][0] else dp[i - 1][0]
+
+        for i in range(1, n):  # base conditon: to reach 1st col, the robot SHOULD MOVE IN DOWN ONLY
+            dp[0][i] = 0 if grid[0][i] else dp[0][i - 1]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if grid[i][j] == 0:
+                    temp = 0
+                    if grid[i][j - 1] == 0:
+                        temp += dp[i][j - 1]
+                    if grid[i - 1][j] == 0:
+                        temp += dp[i - 1][j]
+                    dp[i][j] = temp     # the robot can reach FROM LEFT OR FROM ABOVE to any given cell
+                else:
+                    dp[i][j] = 0
+        print(*dp, sep = "\n")
+        
+        if grid[0][0] == 1 or grid[-1][-1]:
+            return 0
+        else:
+            return dp[-1][-1]
