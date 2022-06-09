9c135fb162e5e7360b3cbeaf571ec7e99bbefc13
cyberkid05 <cyberadithya05@gmail.com>
2022-06-09 15:24:49 +0530

    Time: 6267 ms (10.25%), Space: 81.3 MB (59.84%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        m, n = len(grid1), len(grid1[0])
+        def check(x, y):
+            if x < 0 or x >= m:
+                return 0
+            if y < 0 or y >= n:
+                return 0
+            return 1
+        def fill(x, y):
+            if check(x, y) == 0:
+                return 1
+            if grid2[x][y] == 1 and grid1[x][y] == 0:
+                return 0
+            if grid2[x][y] == 0:
+                return 1
+            grid2[x][y] = 0
+            res = fill(x + 1, y) & fill(x - 1, y) & fill(x, y + 1) & fill(x, y - 1)
+            print("res: ", res)
+            return res
+        
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid1[i][j] == 1 and grid2[i][j] == 1:
+                    # ans += 1
+                    ans += fill(i, j) > 0
+        return ans
