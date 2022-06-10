c1877db6fc2bd433ee53528059567b4c587617cc
cyberkid05 <cyberadithya05@gmail.com>
2022-06-10 09:22:07 +0530

    Time: 420 ms (30.22%), Space: 18.6 MB (8.28%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,16 @@
+import collections
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n= len(nums)
+        hp = collections.defaultdict(lambda:0)
+        ans = 0
+        presum = [0,]
+        for i in range(1, n + 1):
+            presum.append(presum[-1] + nums[i - 1])
+            # print(presum[i])
+            if presum[i] == k:
+                ans += 1
+            if hp[presum[i] - k] > 0:
+                ans += hp[presum[i] - k]
+            hp[presum[i]] += 1
+        return ans
